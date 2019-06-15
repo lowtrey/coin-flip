53085c3ff71344be33b44eabeb8d3db98a03bcaf
Q. Price <priceqp@gmail.com>
2019-06-15 17:44:16 -0500

    Change button styling

--- src/Coin.js
+++ src/Coin.js
@@ -5,7 +5,7 @@ class Coin extends React.Component {
         return (
             <div>
                 <h2>Let's flip a coin!</h2>
-                <img src={`https://tinyurl.com/react-coin-${this.props.side}-jpg`} alt='' /><br />
+                <img src={`https://tinyurl.com/react-coin-${this.props.side}-jpg`} alt='' /><br /><br />
                 <button onClick={this.props.handleClick}>FLIP ME!</button>
                 <p>Out of {this.props.flipCount} flips, there have been {this.props.heads} heads and {this.props.tails} tails.</p>
             </div>
